c0f7ef2b313244e7fb7277831c5ce71f1bf11f4d
Dylan Bowers <dylanvolibowers@gmail.com>
2023-12-09 22:30:15 -0800

    v1.1.0 (not sure why some didnt push)

---
+++ CHANGELOG.md
@@ -0,0 +1,8 @@
+# Affliction Trade Gen Changelog
+
+## 1.1.0
+ - Fixed an issue with tinctures with level requirements
+ - Added support for charms
+
+## 1.0.0
+ - Release
--- src/components/Page.jsx
+++ src/components/Page.jsx
@@ -1,11 +1,16 @@
 import React, { useCallback, useState } from 'react'
 import stringSimilarity from 'string-similarity'
 
-import data from 'util/tinctureMods.json';
+import tinctureData from 'util/tinctureMods.json';
+import charmData from 'util/charmMods.json';
+
 import { generateQuery } from 'util/generateQuery';
 import './Page.css'
 
-const modNameList = data.map(d => d.text);
+const VERSION_NUMBER = '1.1.0';
+
+const tinctureModNameList = tinctureData.map(d => d.text);
+const charmModNameList = charmData.map(d => d.text);
 
 export default function Page() {
   const [ output, setOutput ] = useState('')
@@ -13,21 +18,27 @@ export default function Page() {
   const [ seed, setSeed ] = useState('')
 
   const generateTradeLink = useCallback(() => {
-    const [itemData,,,mods,,] = input.split('--------')
-    const [,,tinctureItemName] = itemData.trim('\n').split('\n')
+    const [itemData,...otherRows] = input.split('--------')
+    const mods = otherRows[otherRows.length - 3]
+    const [,,itemName] = itemData.trim('\n').split('\n')
 
     try {
-      const tinctureBaseMatch = tinctureItemName.match(/\b\w+(?= Tincture)/g)
-      if (tinctureBaseMatch.length !== 1) {
-        throw new Error(`Expected 1 base, got ${tinctureBaseMatch.length}`)
+      let modIds, itemFullSearchName
+      const tinctureBaseMatch = itemName.match(/\b\w+(?= Tincture)/g)
+      const charmBaseMatch = itemName.match(/\b\w+(?= Charm)/g)
+      if (tinctureBaseMatch?.length === 1) {
+        const modMatches = mods.trim('\n').split('\n').map(mod => stringSimilarity.findBestMatch(mod, tinctureModNameList).bestMatch.target)
+        modIds = modMatches.map(mod => tinctureData.find(d => d.text === mod).id)
+        itemFullSearchName = `${tinctureBaseMatch[0]} Tincture`
+      } else if (charmBaseMatch?.length === 1) {
+        const modMatches = mods.trim('\n').split('\n').map(mod => stringSimilarity.findBestMatch(mod, charmModNameList).bestMatch.target)
+        modIds = modMatches.map(mod => charmData.find(d => d.text === mod).id)
+        itemFullSearchName = `${charmBaseMatch[0]} Charm`
+      } else {
+        throw new Error('Could not determine item type');
       }
-      
-      const tinctureFullSearchName = `${tinctureBaseMatch[0]} Tincture`
 
-      const modMatches = mods.trim('\n').split('\n').map(mod => stringSimilarity.findBestMatch(mod, modNameList).bestMatch.target)
-      const modIds = modMatches.map(mod => data.find(d => d.text === mod).id)
-
-      const query = generateQuery(modIds, tinctureFullSearchName)
+      const query = generateQuery(modIds, itemFullSearchName)
 
       console.log(query)
 
@@ -47,8 +58,8 @@ export default function Page() {
 
   return (
     <div className="page-container">
-      <div className="title">Tincture Trade Gen</div>
-      <div className="subtitle">Ctrl+C over your tincture and place it below. Use the "generate" button to then create a trade link.</div>
+      <div className="title">Affliction Trade Gen <a href="https://github.com/ennukee/tinctures/blob/master/CHANGELOG.md" className="version-number">v{VERSION_NUMBER}</a></div>
+      <div className="subtitle">Ctrl+C over your tincture/charm and place it below. Use the "generate" button to then create a trade link.</div>
       <textarea className="input" value={input} onChange={handleChange} />
       <button className="generate-button" onClick={() => generateTradeLink()}>Generate</button>
       {output && <a href={output} target="_blank" rel="noreferrer">Click me to be brought to the trade site (seed: {seed})</a>}
